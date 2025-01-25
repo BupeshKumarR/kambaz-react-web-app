879277599874539750da0dcaaa46fe2b13277847
Bupesh Kumar <bupesh@Bupeshs-MacBook-Pro.local>
2025-01-24 22:07:08 -0500

    updated wd-github

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -8,6 +8,8 @@ export default function TOC() {
         <li><Link to="/Labs/Lab2">Lab 2</Link></li>
         <li><Link to="/Labs/Lab3">Lab 3</Link></li>
         <li><Link to="/Kambaz">Kambaz</Link></li>
+        <li><a href="https://github.com/BupeshKumarR/kambaz-react-web-app.git" id="wd-github">Github</a></li>
+
       </ul>
     );
   }
