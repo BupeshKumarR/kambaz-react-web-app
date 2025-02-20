54f2e82d769073f932b19ed3f512948927cc4dd2
BupeshKumarR <rbupeshkumar@gmail.com>
2025-02-19 22:09:55 -0500

    a3 labs updated

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -23,12 +23,20 @@ import TodoList from "./todo/TodoList";
 import Spreading from "./Spreading";
 import Destructing from "./Destructing";
 import FunctionDestructing from "./FunctionDestructing";
-
+import DestructingImports from "./DestructingImports";
+import Classes from "./Classes";
+import Styles from "./Styles";
+import Add from "./Add";
+import Square from "./Square";
+import Highlight from "./Highlight";
+import AddPathParameters from "./AddPathParameters";
+import PathParameters from "./PathParameters";
 
 export default function Lab3() {
+  console.log('Hello World');
     return (
       <div>
-        <h2>Lab 3</h2>
+        <h2>JavaScript</h2>
         <VariablesAndConstants />
         <VariableTypes />
         <BooleanVariables />
@@ -54,6 +62,23 @@ export default function Lab3() {
         <Spreading />
         <Destructing />
         <FunctionDestructing />
+        <DestructingImports />
+        <Classes />
+        <Styles />
+        <Add a={3} b={4} />
+        <h4>Square of 4</h4>
+        <Square>4</Square>
+        <hr />
+        <Highlight>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+        vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+        </Highlight>
+        <PathParameters />
+        <AddPathParameters />
+
+
+
+
 
 
       </div>
